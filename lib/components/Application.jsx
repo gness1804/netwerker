9f5a2a21b3d7407a491c342e62216609c4ff9063
MilkMan90 <mlkaufman747@gmail.com>
2016-10-27 14:15:34 -0600

    login function

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -3,24 +3,30 @@ import firebase, { contactsFromDatabase, signIn, signOut } from '../firebase';
 import {split, pick, map, extend } from 'lodash';
 import moment from 'moment';
 
+// let contactsFromDatabase;
 
 export default class Application extends Component {
   constructor() {
     super();
     this.state = {
       user: null,
-      contacts: []
+      contacts: [],
+      contactDatabase: null
     }
   }
+
   componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
+    firebase.auth().onAuthStateChanged(user => this.setState({ user, contactDatabase: firebase.database().ref(user.uid) }));
   }
 
+  addNewContact(){
+    this.state.contactDatabase.push('hello');
+  }
 
-// export const contactsFromDatabase = firebase.database().ref('messages');
 
   render() {
     const { user } = this.state;
+    console.log(this.state);
     return(
       <div className = 'application'>
 
@@ -28,6 +34,7 @@ export default class Application extends Component {
           <p>Logged in as <span className="bold">{user.displayName}</span> ({user.email})  <button className='auth-button button' onClick={()=> signOut()}>Sign Out</button>
           </p>
         : <button className='auth-button' onClick={() => signIn()}>Sign In</button> }
+        <button onClick={()=>this.addNewContact()}>Add Contact</button>
         </div>
 
       </div>
