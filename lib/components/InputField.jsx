1f18c6a5a62c4a0391a56637ebbfbd3b54b8c132
MilkMan90 <mlkaufman747@gmail.com>
2016-10-27 15:55:37 -0600

    got basic input functionality

--- lib/components/InputField.jsx
+++ lib/components/InputField.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const InputField = ({className, value, type, handleChange, name}) => {
+export const InputField = ({className, value, type, handleChange, name, objName}) => {
   return(
-    <input className = {className} value = {value} type = {type} name={name} onChange = {(e)=>{handleChange(e, name)}} />
+    <input className = {className} value = {value} type = {type} name={name} onChange = {(e)=>{handleChange(e, name, objName)}} />
   )
 }
--- lib/components/NewContactForm.jsx
+++ lib/components/NewContactForm.jsx
@@ -5,19 +5,49 @@ export default class NewContactForm extends Component {
   constructor() {
     super();
     this.state = {
-
+      firstName: '',
+      lastName: '',
+      companyName: '',
+      numbers: {
+        primary: '',
+        cell: '',
+        work: '',
+        home: ''
+      },
+      emails: {
+        primary:'',
+        secondary: ''
+      },
+      socialMedia: {
+        facebook: '',
+        twitter: '',
+        linkedIn: '',
+        github: '',
+        instagram: ''
+      },
+      notes: ''
     }
   }
 
-  updateState(e, name){
-    this.setState({name: e.target.value})
+  updateState(e, keyName){
+    this.setState({[keyName]: e.target.value})
   }
 
-render(){
-  console.log(this.state);
-  return(
-    <InputField className='firstName-Input' type='text' handleChange={this.updateState.bind(this)}/>
-  )
-}
+  updateStateObject(e, keyName, objName){
+    let object = {[keyName] : e.target.value}
+    this.setState({[objName]: object })
+  }
+
+  render(){
+    console.log(this.state);
+    return(
+      <div className = 'input-field-container'>
+        <InputField className='firstName-Input' type='text' handleChange={this.updateState.bind(this)} name = 'firstName'/>
+        <InputField className='lastName-Input' type='text' handleChange={this.updateState.bind(this)} name = 'lastName'/>
+        <InputField className='companyName-Input' type='text' handleChange={this.updateState.bind(this)} name = 'companyName'/>
+        <InputField className='primaryNumber-Input' type='text' handleChange={this.updateStateObject.bind(this)} objName = 'numbers' name = 'primary'/>
+      </div>
+    )
+  }
 
 }
