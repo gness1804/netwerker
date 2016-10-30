5b05df71d4bbf79dcd896db0b14c9ed2567a8e37
gness1804 <graham.nessler@gmail.com>
2016-10-30 11:49:16 -0600

    input field test passes

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ import { shallow, mount, render } from 'enzyme';
 import { assert,expect } from 'chai';
 
 import Application from '../lib/components/Application';
+import NewContactForm from '../lib/components/NewContactForm';
 
 describe("application", ()=>{
   it("should render as a div", ()=>{
@@ -10,3 +11,13 @@ describe("application", ()=>{
     assert.strictEqual(wrapper.type(), 'div');
   }); //end of render as div
 }); //end of describe application
+
+describe("NewContactForm", ()=>{
+  it("should change state when user enters info in a field", ()=>{
+    const wrapper = mount(<NewContactForm/>);
+    const firstNameInput = wrapper.find('.firstName-Input');
+    assert.strictEqual(wrapper.state('firstName'), '');
+    firstNameInput.simulate('change', {target: {value: 'Hello World'}});
+    assert.strictEqual(wrapper.state('firstName'), 'Hello World');
+  }); //end of change state when user enters info in a field
+}); //end of describe NewContactForm
