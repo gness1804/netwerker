88399a085919f31f4366d0883216f002bf355126
gness1804 <graham.nessler@gmail.com>
2016-10-30 11:37:44 -0600

    test suite setup

---
+++ test/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { assert,expect } from 'chai';
+
+import Application from '../lib/components/Application';
+
+describe("application", ()=>{
+  it("sanity check", ()=>{
+    assert(true);
+  }); //sanity check
+}); //end of desce application
