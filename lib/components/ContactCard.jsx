1a81f6b26d54b89dfd41cf3ce6585bc5b5298628
gness1804 <graham.nessler@gmail.com>
2016-10-30 12:23:59 -0600

    got expansion function to work

--- lib/components/ContactCard.jsx
+++ lib/components/ContactCard.jsx
@@ -4,24 +4,40 @@ import {NewContactForm} from './NewContactForm.jsx'
 export default class ContactCard extends Component {
   constructor() {
     super();
+    this.state = {
+      expanded: false
+    };
   }
+
+  toggleExpand(){
+    this.setState({expanded: !this.state.expanded});
+  }
+
   render() {
     const { firstName, lastName, companyName, numbers, emails, socialMedia, notes } = this.props
+    let display;
+    if (this.state.expanded) {
+      display = (<div className="expanded">
+      <div className='fullname firstName lastName'>{firstName} {lastName}</div>
+      <div className='companyName'>{companyName}</div>
+      <div className = 'cell'>{numbers.cell}</div>
+      <div className = 'home'>{numbers.home}</div>
+      <div className = 'work'>{numbers.work}</div>
+      <div className = 'primary-email'>{emails.primary}</div>
+      <div className = 'secondary-email'>{emails.secondary}</div>
+      <div className = 'facebook'>{socialMedia.facebook}</div>
+      <div className = 'twitter'>{socialMedia.twitter}</div>
+      <div className = 'linkedIn'>{socialMedia.linkedIn}</div>
+      <div className = 'github'>{socialMedia.github}</div>
+      <div className = 'instagram'>{socialMedia.instagram}</div>
+      <div className='notes'>{notes}</div></div>)
+    }
+    else {
+      display = <div className='fullname firstName lastName'>{firstName} {lastName}</div>
+    }
     return(
-      <div className='contactCardContainer'>
-        <div className='fullname firstName lastName'>{firstName} {lastName}</div>
-        <div className='companyName'>{companyName}</div>
-        <div className = 'cell'>{numbers.cell}</div>
-        <div className = 'home'>{numbers.home}</div>
-        <div className = 'work'>{numbers.work}</div>
-        <div className = 'primary-email'>{emails.primary}</div>
-        <div className = 'secondary-email'>{emails.secondary}</div>
-        <div className = 'facebook'>{socialMedia.facebook}</div>
-        <div className = 'twitter'>{socialMedia.twitter}</div>
-        <div className = 'linkedIn'>{socialMedia.linkedIn}</div>
-        <div className = 'github'>{socialMedia.github}</div>
-        <div className = 'instagram'>{socialMedia.instagram}</div>
-        <div className='notes'>{notes}</div>
+      <div className='contactCardContainer' onClick={()=>this.toggleExpand()}>
+        {display}
       </div>
 
     )
