0dc2376f858f12f4d087d676a5be5afa086aba80
ccgamble <christine.c.gamble@gmail.com>
2016-11-02 16:10:12 -0600

    WIP search

--- lib/components/ContactCardList.jsx
+++ lib/components/ContactCardList.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import NewContactForm from './NewContactForm.jsx'
 import ContactCard from './ContactCard.jsx';
+import { filter, includes } from 'lodash';
+import {contains} from 'underscore';
 
 
 export default class ContactCardList extends Component {
   constructor() {
     super();
     this.state = {
-      showFollowupList: false
+      showFollowupList: false,
+      filteredContacts: [],
+      searchString : ""
     };
   }
+
+  searchContacts(e) {
+    this.setState(
+      {searchString: e.target.value});
+    }
+
+
   render() {
 
     let contactArray = this.props.contacts;
@@ -22,14 +33,26 @@ export default class ContactCardList extends Component {
     if(contactArray) {
       contactList =  contactArray.map(c => <ContactCard {...c} user={this.props.user} imgStorage={this.props.imgStorage} contactImgID={c.contactID} contactTextID={c.key} key={c.key} submitEdit = {this.props.submitEdit} toggleFollowup={this.props.toggleFollowup} deleteContact = {this.props.deleteContact}/>)
     }
-    let sortedList = contactList.sort((a, b) => a.props.lastName > b.props.lastName)
+
+
+
+  let sortedList = contactList.sort((a, b) => a.props.lastName > b.props.lastName)
+
+  if (this.state.searchString) {
+      sortedList = filter(sortedList, (contact) => {
+      return includes(contact.props, this.state.searchString)
+   })}
+
 
 
 
     return(
       <div className="contact-card-container">
         <img src="../images/black-flag.svg" className="show-followup-list-button" onClick={()=> {this.setState({showFollowupList: !this.state.showFollowupList})}}/>
+        <input className="search" placeholder="search" onChange={(e)=> {this.searchContacts(e)}}/>
         {sortedList}
+
+
       </div>
     )
   }
